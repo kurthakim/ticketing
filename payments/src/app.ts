08980eedfb00bd42fc2aaab13187868b09924e85
Kurt Hakim <kurthakim@gmail.com>
2022-02-16 20:13:48 +0700

    added payments and payments-mongo kubernetes config file and added payments to skaffold.yaml

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,10 +3,6 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@khticket/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,12 +13,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(currentUser);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
