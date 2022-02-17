24512c55083cb84c7674169782f392a7ed9e7b7b
Kurt Hakim <kurthakim@gmail.com>
2022-02-17 19:06:54 +0700

    added create charge to payments and starting to work on the frontend again

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,7 +8,11 @@ import {
   NotAuthorizedError,
   OrderStatus,
 } from '@khticket/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
+import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -32,7 +36,25 @@ router.post(
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
 
-    res.send({ success: true });
+    const charge = await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
+
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
